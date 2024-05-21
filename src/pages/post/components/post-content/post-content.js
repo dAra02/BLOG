4c4ac33dba83407ff7025cab6662e5319f4f2e12
DEX <you@example.com>
2024-05-21 15:32:04 +0300

    Реализовать редактирование статьи

--- src/pages/post/components/post-content/post-content.js
+++ src/pages/post/components/post-content/post-content.js
@@ -1,21 +1,20 @@
-import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import { H2, Icon } from '../../../../components';
+import { SpecialPanel } from '../special-panel/special-panel';
+import styled from 'styled-components';
 
 const PostContentContainer = ({ className, post: { id, title, imageUrl, content, publichedAt } }) => {
+	const navigate = useNavigate();
+
 	return (
 		<div className={className}>
 			<img src={imageUrl} alt={title} />
 			<H2>{title}</H2>
-			<div className="special-panel">
-				<div className="published-at">
-					<Icon id="fa-calendar-o" margin="0 7px 0 0" size="18px" onClick={() => {}} />
-					{publichedAt}
-				</div>
-				<div className="buttons">
-					<Icon id="fa-pencil-square-o" size="21px" margin="0 10px 0 0" onClick={() => {}} />
-					<Icon id="fa-trash-o" size="21px" onClick={() => {}} />
-				</div>
-			</div>
+			<SpecialPanel
+				publichedAt={publichedAt}
+				margin="-20px 0 20px"
+				editButton={<Icon id="fa-pencil-square-o" size="21px" margin="0 10px 0 0" onClick={() => navigate(`/post/${id}/edit`)} />}
+			/>
 			<div className="post-text">{content}</div>
 		</div>
 	);
@@ -27,28 +26,8 @@ export const PostContent = styled(PostContentContainer)`
 		margin: 0 20px 10px 0;
 	}
 
-	& .special-panel {
-		display: flex;
-		justify-content: space-between;
-		margin: -20px 0 20px;
-	}
-
-	& .published-at {
-		display: flex;
-		align-items: center;
-		font-size: 18px;
-	}
-
-	& i {
-		position: relative;
-		top: -1px;
-	}
-
-	& .buttons {
-		display: flex;
-	}
-
 	& .post-text {
 		font-size: 18px;
+		white-space: pre-line;
 	}
 `;
