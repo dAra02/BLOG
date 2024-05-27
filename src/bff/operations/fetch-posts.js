628cced5f3d3acbe30834cd0e16c27a0483600db
DEX <you@example.com>
2024-05-27 17:27:45 +0300

    Реализовать пагинацию на Главной

--- src/bff/operations/fetch-posts.js
+++ src/bff/operations/fetch-posts.js
@@ -1,14 +1,17 @@
 import { getComments, getPosts } from '../api';
 import { GetCommentsCount } from '../utils';
 
-export const fetchPosts = async () => {
-	const [posts, comments] = await Promise.all([getPosts(), getComments()]);
+export const fetchPosts = async (page, limit) => {
+	const [{ posts, links }, comments] = await Promise.all([getPosts(page, limit), getComments()]);
 
 	return {
 		error: null,
-		res: posts.map((post) => ({
-			...post,
-			commentsCount: GetCommentsCount(comments, post.id),
-		})),
+		res: {
+			posts: posts.map((post) => ({
+				...post,
+				commentsCount: GetCommentsCount(comments, post.id),
+			})),
+			links,
+		},
 	};
 };
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,18 +1,23 @@
 import { useEffect, useState } from 'react';
 import { useServerRequest } from '../../hooks';
-import { PostCard } from './components';
+import { Pagination, PostCard } from './components';
+import { PAGINATION_LIMIT } from '../../constants';
 import styled from 'styled-components';
+import { getLastPageFromLinks } from './utils/get-last-page-from-links';
 
 const MainContainer = ({ className }) => {
 	const [posts, setPosts] = useState([]);
+	const [page, setPage] = useState(1);
+	const [lastPage, setLastPage] = useState(1);
 
 	const requestServer = useServerRequest();
 
 	useEffect(() => {
-		requestServer('fetchPosts').then((posts) => {
-			setPosts(posts.res);
+		requestServer('fetchPosts', page, PAGINATION_LIMIT).then(({ res: { posts, links } }) => {
+			setPosts(posts);
+			setLastPage(getLastPageFromLinks(links));
 		});
-	}, [requestServer]);
+	}, [requestServer, page]);
 
 	return (
 		<div className={className}>
@@ -21,6 +26,7 @@ const MainContainer = ({ className }) => {
 					<PostCard key={id} id={id} title={title} imageUrl={imageUrl} publichedAt={publichedAt} commentsCount={commentsCount} />
 				))}
 			</div>
+			{lastPage > 1 && <Pagination page={page} lastPage={lastPage} setPage={setPage} />}
 		</div>
 	);
 };
